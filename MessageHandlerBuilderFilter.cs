b69950fdbd54d3d88b65edff28fbeed2454579c1
Christoph Hornung <christoph.hornung@this.de>
2024-09-11 17:15:15 +0200

    Add project files.

---
+++ MessageHandlerBuilderFilter.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Http;
+
+namespace HandlerBuilderException;
+
+/// <summary>
+///     An <see cref="IHttpMessageHandlerBuilderFilter" /> which adds the correct DCL certificates.
+/// </summary>
+public class MessageHandlerBuilderFilter : IHttpMessageHandlerBuilderFilter
+{
+	private static bool isFirstCall;
+
+	/// <inheritdoc />
+	public Action<HttpMessageHandlerBuilder> Configure(Action<HttpMessageHandlerBuilder> next)
+	{
+		return builder =>
+		{
+			if (builder.Name == "Test")
+			{
+				builder.PrimaryHandler = GetCustomClientHandler();
+			}
+			
+			next(builder);
+		};
+	}
+
+	private HttpMessageHandler GetCustomClientHandler()
+	{
+		Console.WriteLine("Getting Custom Handler");
+
+		// Some kind of client handler is normally provided here.
+		// To demonstrate the bug we throw on first try.
+		if (!isFirstCall)
+		{
+			isFirstCall = true;
+			Console.WriteLine("First call exception!");
+			throw new Exception("This is a test exception.");
+		}
+
+		Console.WriteLine("No exception");
+
+		HttpClientHandler handler = new();
+		// Some kind of handler modifications are normally done here.
+		return handler;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Http;
+
+namespace HandlerBuilderException;
+
+internal class Program
+{
+	private static async Task Main(string[] args)
+	{
+		await Host.CreateDefaultBuilder(args)
+			.ConfigureServices((hostContext, services) =>
+			{
+				services.AddHttpClient();
+				services.AddSingleton<IHttpMessageHandlerBuilderFilter, MessageHandlerBuilderFilter>();
+				services.AddHostedService<TestBackgroundCaller>();
+			})
+			.RunConsoleAsync();
+	}
+}
+
+internal class TestBackgroundCaller : IHostedService
+{
+	public TestBackgroundCaller(IHttpClientFactory clientFactory)
+	{
+		ClientFactory = clientFactory;
+	}
+
+	public IHttpClientFactory ClientFactory { get; set; }
+
+	public async Task StartAsync(CancellationToken cancellationToken)
+	{
+		while (true)
+		{
+			Console.WriteLine("Calling HttpClient");
+			
+			try
+			{
+				using HttpClient client = ClientFactory.CreateClient("Test");
+				await client.GetAsync("https://www.google.com", cancellationToken);
+				Console.WriteLine("Called HttpClient");
+			}
+			catch (Exception e)
+			{
+				// Just ignore
+				Console.WriteLine("Exception HttpClient");
+			}
+
+			await Task.Delay(1000, cancellationToken);
+		}
+	}
+
+	public Task StopAsync(CancellationToken cancellationToken)
+	{
+		return Task.CompletedTask;
+	}
+}
